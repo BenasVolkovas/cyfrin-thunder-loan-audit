5312a7f0697cfef825454577d0ed2de7e093e85a
Benas Volkovas <benasvol@gmail.com>
2024-01-24 22:00:25 +0200

    docs: interfaces audit

--- src/interfaces/IThunderLoan.sol
+++ src/interfaces/IThunderLoan.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
+// @todo @audit this interface is not used anywhere
 interface IThunderLoan {
+    // @todo @audit this function takes address token, but contract uses IERC20 interface
     function repay(address token, uint256 amount) external;
 }
