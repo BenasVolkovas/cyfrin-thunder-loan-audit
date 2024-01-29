84fa96c4324e1c31c547e91538cbe83529f8c679
Benas Volkovas <benasvol@gmail.com>
2024-01-29 19:53:36 +0200

    test: started oracle manipulation test

--- audit-data/findings.md
+++ audit-data/findings.md
@@ -23,8 +23,64 @@ However, the `deposit` function in the `ThunderLoan` contract uses the `updateEx
     }
 ```
 
-**Impact:**
+**Impact:** There are several impacts of this bug:
+
+1. The `ThunderLoan::redeem` function is blocked from redeeming the total liquidity provider balance, because the protocol wants to transfer more than it should.
+2. Rewards are calculated incorrectly, leading to liquidity providers getting more rewards than they should.
 
 **Proof of Concept:**
 
-**Recommended Mitigation:**
+1. LP deposits
+2. User takes out a flash loan
+3. LP tries to redeem their funds
+4. The redeem function reverts
+
+Place the following into `ThunderLoan.t.sol`:
+
+```javascript
+function test_audit_RedeemsAfterLoan() public setAllowedToken hasDeposits {
+    uint256 amountToBorrow = AMOUNT * 10;
+    uint256 calculatedFee = thunderLoan.getCalculatedFee(
+        tokenA,
+        amountToBorrow
+    );
+
+    vm.startPrank(user);
+    tokenA.mint(address(mockFlashLoanReceiver), calculatedFee);
+    thunderLoan.flashloan(
+        address(mockFlashLoanReceiver),
+        tokenA,
+        amountToBorrow,
+        ""
+    );
+    vm.stopPrank();
+
+    uint256 amountToRedeem = type(uint256).max;
+
+    vm.startPrank(liquidityProvider);
+    thunderLoan.redeem(tokenA, amountToRedeem);
+    vm.stopPrank();
+}
+```
+
+**Recommended Mitigation:** Remove the incorrectly used `updateExchangeRate` function call from the `deposit` function.
+
+```diff
+    function deposit(
+        IERC20 token,
+        uint256 amount
+    ) external revertIfZero(amount) revertIfNotAllowedToken(token) {
+        AssetToken assetToken = s_tokenToAssetToken[token];
+        uint256 exchangeRate = assetToken.getExchangeRate();
+        uint256 mintAmount = (amount * assetToken.EXCHANGE_RATE_PRECISION()) /
+            exchangeRate;
+        emit Deposit(msg.sender, token, amount);
+
+        assetToken.mint(msg.sender, mintAmount);
+
+-       uint256 calculatedFee = getCalculatedFee(token, amount);
+-       assetToken.updateExchangeRate(calculatedFee);
+
+        token.safeTransferFrom(msg.sender, address(assetToken), amount);
+    }
+```
--- test/unit/ThunderLoanTest.t.sol
+++ test/unit/ThunderLoanTest.t.sol
@@ -1,10 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-import {Test, console} from "forge-std/Test.sol";
+import {Test, console2} from "forge-std/Test.sol";
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 import {BaseTest, ThunderLoan} from "./BaseTest.t.sol";
 import {AssetToken} from "../../src/protocol/AssetToken.sol";
+import {IFlashLoanReceiver} from "../../src/interfaces/IFlashLoanReceiver.sol";
 import {MockFlashLoanReceiver} from "../mocks/MockFlashLoanReceiver.sol";
+import {ERC20Mock} from "../mocks/ERC20Mock.sol";
+import {BuffMockPoolFactory} from "../mocks/BuffMockPoolFactory.sol";
+import {BuffMockTSwap} from "../mocks/BuffMockTSwap.sol";
 
 contract ThunderLoanTest is BaseTest {
     uint256 constant AMOUNT = 10e18;
@@ -132,5 +137,51 @@ contract ThunderLoanTest is BaseTest {
         vm.startPrank(liquidityProvider);
         thunderLoan.redeem(tokenA, amountToRedeem);
         vm.stopPrank();
+    }
+
+    function test_audit_ManipulatesOraclePrice() public {
+        // 1. Setup contracts
+        tokenA = new ERC20Mock();
+        BuffMockPoolFactory mockPoolFactory = new BuffMockPoolFactory(
+            address(weth)
+        );
+        address tSwapPool = mockPoolFactory.createPool(address(tokenA));
+
+        thunderLoan = new ThunderLoan();
+        proxy = new ERC1967Proxy(address(thunderLoan), "");
+        thunderLoan = ThunderLoan(address(proxy));
+        thunderLoan.initialize(address(mockPoolFactory));
+
+        // 2. Fund T-Swap
+        uint256 lpFunds = 100e18;
+        vm.startPrank(liquidityProvider);
+        tokenA.mint(liquidityProvider, lpFunds);
+        tokenA.approve(address(tSwapPool), lpFunds);
+        weth.mint(liquidityProvider, lpFunds);
+        weth.approve(address(tSwapPool), lpFunds);
+        BuffMockTSwap(tSwapPool).deposit(
+            lpFunds,
+            lpFunds,
+            lpFunds,
+            block.timestamp
+        );
+        vm.stopPrank();
+
+        // 3. Fund ThunderLoan
+        vm.prank(thunderLoan.owner());
+        thunderLoan.setAllowedToken(tokenA, true);
+
+        vm.startPrank(liquidityProvider);
+        tokenA.mint(liquidityProvider, 1000e18);
+        tokenA.approve(address(thunderLoan), 1000e18);
+        thunderLoan.deposit(tokenA, 1000e18);
+        vm.stopPrank();
+
+        // 4. Take out 2 flashloans
+        uint256 normalFeeCost = thunderLoan.getCalculatedFee(tokenA, 100e18);
+        console2.log("Normal fee cost: ", normalFeeCost);
+        // 0.296147410319118389
+
+        uint256 amoutToBorrow = 50e18;
     }
 }
